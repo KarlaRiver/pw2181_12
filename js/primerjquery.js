68bd963eae3985a8ce6338a076ac488fe40e204e
CCDM11 <joseinzunzam2@gmail.com>
2018-04-24 12:55:00 -0600

    primer jQuery

---
+++ js/primerjquery.js
@@ -0,0 +1,20 @@
+var inicia = function(){
+    //JSON = JavaScript Object Notation
+    
+     var nuevo = function(){
+        $.ajax({
+            url: 'https://randomuser.me/api/',
+            dataType: 'json',
+            success: function(data) {
+                $("#nombre").html(data.results[0].name.first+" "+data.results[0].name.last);
+             //console.log(data);
+             //Para la foto;D
+                $("#foto").attr("src",data.results[0].picture.large)
+            }
+          });
+     }
+     $("#nuevo").on("click",nuevo);
+}
+
+$(document).ready(inicia);
+
