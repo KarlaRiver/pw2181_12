91c88fc06d708d9b23ec4c2ac4bb0ae7d5a69723
CCDM11 <joseinzunzam2@gmail.com>
2018-04-26 12:54:58 -0600

    Marvel Comics

--- marvel/js/index.js
+++ marvel/js/index.js
@@ -4,6 +4,10 @@ var marvel = function(){
         var url="http://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith="
         var cantidadComics = 0;
         var comics = "";
+        var cantidadSeries =0;
+        var series ="";
+        var cantidadHistorias=0;
+        var historias="";
         url = url + personaje;
         $.ajax({
             dataType:"json",
@@ -12,11 +16,43 @@ var marvel = function(){
                 if(response.code == 200){
                     $("#foto").attr("src",response.data.results[0].thumbnail.path+"."+
                     response.data.results[0].thumbnail.extension);
+                    $("#nombre").html(response.data.results[0].name);
+                    cantidadComics=response.data.results[0].comics.returned;
+                    for(var i=0;i<cantidadComics;i++){
+                        comics+=response.data.results[0].comics.items[i].name+"<br>";
+                    }
+                    $("#comics").html("Comics: <br>"+comics);
+
+                    cantidadSeries=response.data.results[0].series.returned;
+                    for(var i=0;i<cantidadSeries;i++){
+                        series+=response.data.results[0].series.items[i].name+"<br>";
+                    }
+                    $("#serie").html("Series: <br>"+series);
+
+                    cantidadHistorias=response.data.results[0].stories.returned;
+                    for(var i=0;i<cantidadHistorias;i++){
+                        historias+=response.data.results[0].stories.items[i].name+"<br>";
+                    }
+                    $("#historia").html("Historias: <br>"+historias);
+
+
+                    
                 }
             }
         });
     }
+
+    var teclaPersonaje = function(tecla){
+        //Enter = 10+13
+        //Retorno de carro y avance de linea
+        if(tecla.which == 13){ //El 13 es un enter
+            Buscar();
+        }
+
+    }
+
     $("#btnBuscar").on("click",Buscar);
+    $("#txtPersonaje").on("keypress",teclaPersonaje);
 }
 
 $(document).ready(marvel);
