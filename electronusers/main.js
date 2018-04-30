09936943a8b438f705a944fccb2142159c2db501
CCDM11 <ccdm11@CCDM11.local>
2018-04-30 13:04:45 -0600

    Eletron1

---
+++ electronusers/main.js
@@ -0,0 +1,25 @@
+//Aplicacion general:
+const app=require('electron').app;
+//Uso de las pantallas del sistema:
+const BrowserWindow=require('electron').BrowserWindow;
+//Ruta de la carpeta base
+const path=require('path');
+//URL de las páginas
+const url=require('url');
+
+//ECMASCRIPT 6 -JS
+let PantallaPrincipal;
+
+function muestraPantallaPrincipal(){
+    PantallaPrincipal=new BrowserWindow({width:620,heigth:825});
+    PantallaPrincipal.loadURL(url.format({
+        //join: concatenar cadenas
+        pathname:path.join(__dirname,'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+    PantallaPrincipal.show();
+}
+app.on('ready',muestraPantallaPrincipal);
+
+
